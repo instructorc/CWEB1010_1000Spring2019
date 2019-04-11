8a607e28c003f3b09b1f4f0315665f837780be8d
cifulton <cfulton@dunwoody.edu>
2019-04-11 08:25:24 -0500

    implement js all three ways

---
+++ js_one/js/change_dom.js
@@ -0,0 +1,4 @@
+document.getElementById('button').onclick = function(){
+    document.getElementById('button').id = "button_two";
+    document.getElementsByTagName('img')[0].src="https://i.pinimg.com/originals/f0/6a/23/f06a23908b4477d19dfa6aedd247b6d7.jpg";
+};
